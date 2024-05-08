9544517575603e6c4707f267f38776c305e04628
Md. Rakibul Islam <rakibrohan54@gmail.com>
2024-05-08 11:35:37 +0600

    data ingestion part correction final

--- srce/dsprojects1/components/data_ingestion.py
+++ srce/dsprojects1/components/data_ingestion.py
@@ -27,11 +27,9 @@ class DataIngestion:
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
-
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
-            df.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
-            df.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
-            logging.info("data ingestion is complete")
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
             
             return(
                 self.ingestion_config.train_data_path,
