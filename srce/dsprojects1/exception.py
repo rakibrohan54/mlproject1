8c7f3158c1512b3cbb9e9a8adb842b660208c968
Md. Rakibul Islam <rakibrohan54@gmail.com>
2024-05-08 10:30:51 +0600

    srce

--- srce/dsprojects1/exception.py
+++ srce/dsprojects1/exception.py
@@ -1,5 +1,5 @@
 import sys
-from src.dsprojects1.logger import logging
+from srce.dsprojects1.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
