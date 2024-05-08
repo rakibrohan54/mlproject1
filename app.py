a62e0d4886af3766c58b568015fb1f6ec9e011ed
Md. Rakibul Islam <rakibrohan54@gmail.com>
2024-05-08 11:22:53 +0600

    ..

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ if __name__=="__main__":
     try:
         #data_ingestion_config=DataIngestionConfig()
         data_ingestion=DataIngestion()
-        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+        data_ingestion.initiate_data_ingestion()
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
