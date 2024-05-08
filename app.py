7caf4a6b0e466d4af28f83b39011b2402559c0a1
Md. Rakibul Islam <rakibrohan54@gmail.com>
2024-05-08 10:27:17 +0600

    src to srce rename

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from src.dsprojects1.logger import logging
-from src.dsprojects1.exception import CustomException
-
-
-
 import sys
+print(sys.path)
 
-
+from srce.dsprojects1.logger import logging
+from srce.dsprojects1.exception import CustomException
+from srce.dsprojects1.components.data_ingestion import DataIngestion
+from srce.dsprojects1.components.data_ingestion import DataIngestionConfig
 
 
 
@@ -13,7 +12,9 @@ if __name__=="__main__":
     logging.info("The execution has started")
 
     try:
-        a=1/0
+        # data_ingestion_config=DataIngestionConfig()
+        data_ingestion=DataIngestion
+        data_ingestion.initiate_data_ingestion()
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
---
+++ srce/dsprojects1/utils.py
@@ -0,0 +1,32 @@
+import os
+import sys
+from src.dsprojects1.exception import CustomException
+from src.dsprojects1.logger import logging
+import pandas as pd
+from dotenv import load_dotenv
+import pymysql
+
+
+load_dotenv()
+
+host=os.getenv("host")
+user=os.getenv("user")
+password=os.getenv("password")
+db=os.getenv('db')
+
+def raed_sql_data():
+    logging.info("Reading SQL database started")
+    try:
+      mydb=pymysql.connect(
+         host=host,
+         user=user,
+         password=password,
+         db=db
+      )
+      logging.info("connection Establish",mydb)
+      df=pd.read_sql_query('Select * from students',mydb)
+      print(df.haed())
+
+      return df
+    except Exception as ex:
+       raise CustomException(ex)
